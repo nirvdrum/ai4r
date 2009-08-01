911031eea0de3dce43028d80f5d06a4e5b37985f
Kevin Menard <nirvdrum@gmail.com>
2009-08-01 08:40:24 -0400

    Start using jeweler for gem management.

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,6 +5,26 @@ require 'rake/gempackagetask'
 
 load 'ai4r.gemspec'
 
+begin
+  require 'jeweler'
+  Jeweler::Tasks.new do |gemspec|
+    gemspec.name = "ai4r"
+    gemspec.description = "Describe your gem"
+    gemspec.email = "sergio@jadeferret.com"
+    gemspec.homepage = "http://ai4r.rubyforge.org"
+    gemspec.authors = ["Sergio Fierens"]
+    gemspec.rubyforge_project = "ai4r"
+    gemspec.summary = <<-DESC.strip.gsub(/\n\s+/, " ")
+    Ruby implementations of algorithms covering several
+    Artificial intelligence fields, including Genetic
+    algorithms, Neural Networks, machine learning, and clustering.
+  DESC
+  end
+rescue LoadError
+  puts "Jeweler not available. Install it with: sudo gem install technicalpickles-jeweler -s http://gems.github.com"
+end
+
+
 Rake::TestTask.new do |t|
   t.test_files = FileList["test/**/*_test.rb"]
 end
